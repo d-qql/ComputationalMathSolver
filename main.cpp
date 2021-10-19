c5da247886bcc2a6a5612ca958f3a4809af14711
d-qql <petrov.dmitrii@phystech.edu>
2021-10-19 09:16:54 +0300

    task1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-
+// https://github.com/d-qql/ComputationalMathSolver
 #include "Interpolation/Newton.hpp"
 // реализуем функцию, которую необходимо заменить полиномом
 double func(double x) {
-    return std::exp(x)*sin(x);
+    return sin(x);
 }
 
 int main() {
     // создаем интерполятор Ньютона, указав крайние точки отрезка(тут [-1, 1])
     // и необходимое число узлов, а также нашу функцию
-    Newton newton(2, 4, 4, func);
+    Newton newton(0, 5, 3, func);
     // выводим интерполяционный полином
     newton.get_polynomial();
-    std::cout<<newton.interpolate(3);
+    std::cout << "Константа Лебега ~ 2/Pi * ln(2) => Ошибка <= sup(|deltaF|) * L = 2 * 2/Pi * ln(2)";
     return 0;
 }
